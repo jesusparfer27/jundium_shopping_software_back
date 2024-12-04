2896df5beb3bd76c9113b54fbcc6e8c21ab64f75
jesusparfer27 <jesusparfer@alumnos.cei.es>
2024-12-04 01:44:39 +0100

    commit

--- back/config/mongo.config.js
+++ back/config/mongo.config.js
@@ -6,11 +6,11 @@ export const __dirname = path.resolve()
 
 dotenv.config();
 
-export const FULL_DOMAIN_1 = process.env.FULL_DOMAIN_1 || 'http://localhost:3027/API/v1';
+export const FULL_DOMAIN_1 = process.env.FULL_DOMAIN_1 || 'http://localhost:3027';
 
 export const PORT = process.env.PORT || 3027
 
-export const JWT_SECRET = process.env.JWT_SECRET || 'S4jIRHqt'
+export const JWT_SECRET = process.env.JWT_SECRET || 'S16jIRHqt'
 
 // Configuración de MongoDB
 export const mongodbUri = process.env.MONGODB_URI;
